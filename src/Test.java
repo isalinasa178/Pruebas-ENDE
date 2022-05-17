37cc5dbc899194562739c7e1907264cddbc5c30b
isalinaagu <salinaagu@salesianospamplona.net>
2022-05-17 10:53:23 +0200

    Primer commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Test {
+    private static int[] numeros = {-3, 2, 8, 12, 17, 29, 44, 58, 79};
+
+    public static void main(String[] args) {
+        System.out.println("Hola donovan");
+    }
+}
+
