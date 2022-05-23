677a3d85d2dd5f2b53f1ea7b330d7b0d05fef311
isalinasa178 <90615715+isalinasa178@users.noreply.github.com>
2022-05-23 17:35:26 +0200

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -6,7 +6,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hola donovan");
          System.out.println("Hola carlos");
-        System.out.println("Hola carlos");
+        System.out.println("Hola ");
     }
 }
 
