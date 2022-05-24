7ed681491285aa839d8204b516cc3bb2ffa09daa
dyaguanab <90379199+dyaguanab@users.noreply.github.com>
2022-05-24 13:26:46 +0200

    cambiado main

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hola donovan");
-         System.out.println("Hola carlitas conde");
+         System.out.println("Hola clase");
     }
 }
 
